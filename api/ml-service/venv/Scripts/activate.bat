538d33e512fcc37d59efbc38b08837e8f72daef5
SdSingh10 <133514216+SdSingh10@users.noreply.github.com>
2025-08-10 14:29:31 +0530

    aws connections 2.6

--- api/ml-service/venv/Scripts/activate.bat
+++ api/ml-service/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\chawl\OneDrive\Desktop\Shubham\Web designing\movie-recommendation\CINEMATE\ml-service\venv
+set VIRTUAL_ENV=C:\Users\chawl\OneDrive\Desktop\Shubham\Web designing\movie-recommendation\CINEMATE\api\ml-service\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
